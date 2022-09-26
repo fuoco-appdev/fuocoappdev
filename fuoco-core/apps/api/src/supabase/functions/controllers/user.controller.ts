fb83b11b395946c1a5260284c3482a138e23800b
lucasfuoco <lucasfuoco@gmail.com>
2022-09-25 21:17:36 -0400

    Modify user service and controller

--- fuoco-core/apps/api/src/supabase/functions/controllers/user.controller.ts
+++ fuoco-core/apps/api/src/supabase/functions/controllers/user.controller.ts
@@ -4,39 +4,84 @@ import {Controller, Post, Guard, ContentType} from 'https://fuoco-appdev-core-ap
 import * as Oak from "https://deno.land/x/oak@v11.1.0/mod.ts";
 import UserService from '../services/user.service.ts';
 import { AuthGuard } from '../guards/index.ts';
+import { User, Location } from '../protobuf/core_pb.js';
+import * as HttpError from "https://deno.land/x/http_errors@3.0.0/mod.ts";
 
 @Controller('/user')
 export class UserController {
     @Post('/:id')
     @Guard(AuthGuard)
-    @ContentType('x-protobuf')
+    @ContentType('application/x-protobuf')
     public getUser(context: Oak.RouterContext<string, Oak.RouteParams<string>, Record<string, any>>): void {
-        context.response.body = JSON.stringify({});
+        const paramsId = context.params['id'];
+        let data = null;
+        UserService.findAsync(paramsId).then((value: any | null) => {
+            data = value;
+        });
+        if (!data) {
+            throw HttpError.createError(404, `User with superbase id ${paramsId} not found`);
+        }
+
+        const id = data['id'];
+        const createdAt = data['created_at'];
+        const supabaseId = data['supabase_id'];
+        const role = data['role'];
+        const company = data['company'];
+        const email = data['email'];
+        const phoneNumber = data['phone_number'];
+        const language = data['language'];
+        const locationData = data['location'];
+        const requestStatus = data['request_status'];
+        const appData = data['apps'];
+
+        const user = new User();
+        const location = new Location();
+        if (locationData) {
+            location.setLatitude(Number(locationData['latitude']));
+            location.setLongitude(Number(locationData['longitude']));
+        }
+        
+        user.setId(id);
+        user.setCreatedAt(createdAt);
+        user.setSupabaseId(supabaseId);
+        user.setRole(role);
+        user.setCompany(company);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setLanguage(language);
+        user.setLocation(location);
+        user.setRequestStatus(requestStatus);
+        user.setAppsList([]);
+
+        context.response.type = "application/x-protobuf";
+        context.response.body = user.serializeBinary();
     }
 
-    @Post('/create/:id')
+    @Post('/create')
     @Guard(AuthGuard)
-    @ContentType('x-protobuf')
+    @ContentType('application/x-protobuf')
     public createUser(context: Oak.RouterContext<string, Oak.RouteParams<string>, Record<string, any>>): void {
+        const body = context.request.body();
+        const user = User.deserializeBinary(body.value);
         context.response.body = JSON.stringify({});
     }
 
     @Post('/all')
-    @ContentType('x-protobuf')
+    @ContentType('application/x-protobuf')
     public getAllUsers(context: Oak.RouterContext<string, Oak.RouteParams<string>, Record<string, any>>): void {
         context.response.body = JSON.stringify({});
     }
 
     @Post('/update/:id')
     @Guard(AuthGuard)
-    @ContentType('x-protobuf')
+    @ContentType('application/x-protobuf')
     public updateUser(context: Oak.RouterContext<string, Oak.RouteParams<string>, Record<string, any>>): void {
         context.response.body = JSON.stringify({});
     }
 
     @Post('/delete/:id')
     @Guard(AuthGuard)
-    @ContentType('x-protobuf')
+    @ContentType('application/x-protobuf')
     public deleteUser(context: Oak.RouterContext<string, Oak.RouteParams<string>, Record<string, any>>): void {
         context.response.body = JSON.stringify({});
     }
--- fuoco-core/apps/api/src/supabase/functions/services/user.service.ts
+++ fuoco-core/apps/api/src/supabase/functions/services/user.service.ts
@@ -1,5 +1,117 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import SupabaseService from "./supabase.service.ts";
+import { User, UserRequestStatus } from '../protobuf/core_pb.js';
+
 export class UserService {
-    
+    public async findAsync(supabaseId: string): Promise<any | null> {
+        const {data, error} = await SupabaseService.client
+            .from('users')
+            .select()
+            .eq('supabase_id', supabaseId)
+            .single();
+
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return data;
+    }
+
+    public async createAsync(user: InstanceType<typeof User>): Promise<any | null> {
+        const supabaseId = user.getSupabaseId();
+        const role = user.getRole();
+        const company = user.getCompany();
+        const email = user.getEmail();
+        const phoneNumber = user.getPhoneNumber();
+        const language = user.getLanguage();
+        const location = user.getLocation();
+        
+        const {data, error} = await SupabaseService.client
+            .from('users')
+            .insert([{
+                supabase_id: supabaseId,
+                ...(role && {role: role}),
+                ...(company && {company: company}),
+                ...(email && {email: email}),
+                ...(phoneNumber && {phone_number: phoneNumber}),
+                ...(language && {language: language}),
+                ...(location && {location: {
+                    latitude: location?.getLatitude(),
+                    longitude: location?.getLongitude()
+                }}),
+                request_status: UserRequestStatus.IDLE,
+                apps: {apps: []}
+            }])
+            .single();
+
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return data;
+    }
+
+    public async updateAsync(user: InstanceType<typeof User>): Promise<any | null> {
+        const company = user.getCompany();
+        const email = user.getEmail();
+        const phoneNumber = user.getPhoneNumber();
+        const language = user.getLanguage();
+        const location = user.getLocation();
+        const requestStatus = user.getRequestStatus();
+        const apps = user.getAppsList();
+        const {data, error} = await SupabaseService.client
+            .from('users')
+            .update([{ 
+                ...(company && {company: company}),
+                ...(email && {email: email}),
+                ...(phoneNumber && {phone_number: phoneNumber}),
+                ...(language && {language: language}),
+                ...(location && {location: {
+                    latitude: location?.getLatitude(),
+                    longitude: location?.getLongitude()
+                }}),
+                ...(requestStatus && {request_status: requestStatus}),
+                ...(apps && {apps: {apps: apps }})
+            }])
+            .match({id: user.getId()});
+
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return data;
+    }
+
+    public async findAllAsync(): Promise<any[] | null> {
+        const {data, error} = await SupabaseService.client
+        .from('users')
+        .select();
+
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return data;
+    }
+
+    public async deleteAsync(supabaseId: string): Promise<any | null> {
+        const {data, error} = await SupabaseService.client
+        .from('users')
+        .delete()
+        .match({supabase_id: supabaseId})
+        .single();
+
+        if (error) {
+            console.error(error);
+            return null;
+        }
+
+        return data;
+    }
 }
 
 export default new UserService();
