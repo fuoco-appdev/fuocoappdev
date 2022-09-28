d9f9b99cdce7bc68a63c94afbcd7d9c03cd6a62c
lucasfuoco <lucasfuoco@gmail.com>
2022-09-27 22:27:18 -0400

    Remove console.log from supabase service in api

--- fuoco-core/apps/api/src/supabase/functions/services/supabase.service.ts
+++ fuoco-core/apps/api/src/supabase/functions/services/supabase.service.ts
@@ -7,7 +7,6 @@ class SupabaseService {
     constructor() {
         const url = Deno.env.get('SUPABASE_URL');
         const key = Deno.env.get('SUPABASE_SERVICE_ROLE_KEY');
-        console.log(Deno.env.get('SUPABASE_ANON_KEY'));
         if (!url) {
             throw new Error("SUPABASE_URL doesn't exist");
         }
