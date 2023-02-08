e1f8a6c0b055b81a05f787c2b2c2bd19ee5f32a9
lucasfuoco <lucasfuoco@gmail.com>
2023-02-08 14:55:37 -0500

    Add android and ios projects to app

---
+++ apps/app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fuocoappdev.app',
+  appName: 'fuoco.appdev',
+  webDir: '../../dist/apps/app',
+  bundledWebRuntime: false,
+};
+
+export default config;
