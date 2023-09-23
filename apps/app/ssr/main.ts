89d09bb4d7e08fc40d3c5431960e3967343562e7
Lucas Fuoco <fuoco.appdev@gmail.com>
2023-09-23 09:06:33 +0400

    Fix initial server html

--- apps/app/ssr/main.ts
+++ apps/app/ssr/main.ts
@@ -22,7 +22,7 @@ const mainPath = path.resolve(__dirname, '../server/main.js');
 const indexHtmlPath = path.resolve(__dirname, '../client/index.html');
 router.use(express.static(path.resolve(__dirname, '../client')));
 //router.use(express.static(path.resolve(__dirname, '../server')));
-router.use('*', (req, res) => {
+router.get('*', (req, res) => {
   let indexData = fs.readFileSync(indexHtmlPath, 'utf8');
   // Skip caching ssr fragment in dev mode.
   if (process.env['NODE_ENV'] !== 'production') {
